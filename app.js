f17036fe918dbdf7fbe22f6691a4e9ba3eed109a
Axonn1018 <ktruong1018@gmail.com>
2024-06-01 19:45:06 -0400

    Adding javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+function testButton() {
+    //console.log("Testing script for button press");
+    var x = document.getElementById("buttonMsg");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function nameClick() {
+    console.log("Clicked the name on the top");
+}
+
+// Opening new link on button link via JS
+// "_blank" allows to open in new tab
+function githubOpen(){
+    window.open("https://github.com/ktruong1018", "_blank");
+}
