c8bb1bc71383e3bffc9679a1ee62a5e1e13d1d9b
Axonn1018 <ktruong1018@gmail.com>
2024-06-03 07:24:56 -0400

    Cleaned up bootstrap and undeeded code

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 function testButton() {
     //console.log("Testing script for button press");
     var x = document.getElementById("buttonMsg");
